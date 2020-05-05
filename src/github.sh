74329a739d90f6b6da1cabb98db4162ef9275bcd
Rafa Gómez <rgomezcasas@gmail.com>
2020-05-05 13:56:50 +0200

    Add support for emojis in labels

--- src/github.sh
+++ src/github.sh
@@ -18,9 +18,8 @@ github::add_label_to_pr() {
 
   local -r body=$(curl -sSL -H "Authorization: token $GITHUB_TOKEN" -H "$GITHUB_API_HEADER" "$GITHUB_API_URI/repos/$GITHUB_REPOSITORY/pulls/$1")
   local labels=$(echo "$body" | jq .labels | jq -r ".[] | .name" | grep -v "size/")
-  labels+=("$label_to_add")
-
-  local -r comma_separated_labels=$(github::format_labels "${labels[@]/#/}")
+  labels=$(printf "%s\n%s" "$labels" "$label_to_add")
+  local -r comma_separated_labels=$(github::format_labels "$labels")
 
   log::message "Final labels: $comma_separated_labels"
 
@@ -34,11 +33,16 @@ github::add_label_to_pr() {
 }
 
 github::format_labels() {
+  SAVEIFS=$IFS
+  IFS=$'\n'
+  local -r labels=($@)
+  IFS=$SAVEIFS
   quoted_labels=()
-  for label in "${@}"; do
-    if [ -n "$label" ]; then
-      quoted_labels+=("$(str::quote "$label")")
-    fi
+
+  for ((i = 0; i < ${#labels[@]}; i++)); do
+    #    echo "Label $i: ${labels[$i]}"
+    label="${labels[$i]}"
+    quoted_labels+=("$(str::quote "$label")")
   done
 
   coll::join_by "," "${quoted_labels[@]/#/}"
