10b05ceaa40cc5c33a2d082699bbc42ccf5a4f60
Rafa Gómez <rgomezcasas@gmail.com>
2022-03-20 11:27:49 +0100

    test: add a big file ignoring it

--- src/github.sh
+++ src/github.sh
@@ -3,9 +3,9 @@
 GITHUB_API_HEADER="Accept: application/vnd.github.v3+json"
 
 github::calculate_total_modifications() {
-  local -r body=$(curl -sSL -H "Authorization: token $GITHUB_TOKEN" -H "$GITHUB_API_HEADER" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$1")
   local -r pr_number="${1}"
   local -r files_to_ignore="${2}"
+
   if [ -z "$files_to_ignore" ]; then
     local -r body=$(curl -sSL -H "Authorization: token $GITHUB_TOKEN" -H "$GITHUB_API_HEADER" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$pr_number")
 
