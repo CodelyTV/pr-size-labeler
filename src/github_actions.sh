8e7d1714298637a2cc8f7b6602ab97cdff4dafe5
johnlk <john@useintegral.com>
2024-04-13 21:06:57 -0400

    Support ignore deletions with "ignore_file_deletions" param
    Test files_to_ignore code path
    
    Tweak the variable type
    
    Correct bad logic in the file ignore block
    
    Debug
    
    More debugging
    
    More debugging
    
    Fingers crossed
    
    Another try
    
    Last try
    
    Lets see if this one does it
    
    Testing again
    
    More tests
    
    More testing
    
    Last test
    
    Wait, I might be onto something
    
    Put some changes back
    
    Fixes
    
    Whoops
    
    Walk back yml test

--- src/github_actions.sh
+++ src/github_actions.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 github_actions::get_pr_number() {
-  jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH"
+  local -r pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+  if [[ "$pull_request_number" != "null" ]]; then
+    echo "$pull_request_number"
+  else
+    echo "Not a pull request event"
+    exit 1
+  fi
 }
