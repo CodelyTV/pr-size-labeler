cc6827c0ee19ec28fbdd07bdf4e4dc1fd484e0ad
John Kuhn <john@useintegral.com>
2024-05-14 06:15:36 -0700

    Support ignoring file deletions (#76)
    * Support ignoring file deletions
    
    Tweak action.yml
    
    * Tweak the readme description
    
    Co-authored-by: Onkar Ruikar <87750369+OnkarRuikar@users.noreply.github.com>
    
    * tweak removed file check to include line_deletion var
    
    Co-authored-by: Onkar Ruikar <87750369+OnkarRuikar@users.noreply.github.com>
    
    * docs: unify the "Basic concepts or assumptions" readme section into the most related arguments to make it easier to read
    
    Improve the existing contribution guide (#74)
    
    * Improve the existing contribution guide
    
    * reorder the contribution steps to push testing up
    
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>
    
    * tweak contribution process opener copy
    
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>
    
    ---------
    
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>
    
    Put back accidental deletion
    
    ---------
    
    Co-authored-by: Onkar Ruikar <87750369+OnkarRuikar@users.noreply.github.com>
    Co-authored-by: Javier Ferrer González <javier.ferrer@codely.com>
    Co-authored-by: Rob W <rlw0788@gmail.com>

--- src/labeler.sh
+++ src/labeler.sh
@@ -10,9 +10,10 @@ labeler::label() {
   local -r message_if_xl="${11}"
   local -r files_to_ignore="${12}"
   local -r ignore_line_deletions="${13}"
+  local -r ignore_file_deletions="${14}"
 
   local -r pr_number=$(github_actions::get_pr_number)
-  local -r total_modifications=$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions")
+  local -r total_modifications=$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")
 
   log::message "Total modifications (additions + deletions): $total_modifications"
   log::message "Ignoring files (if present): $files_to_ignore"
--- tests/github_test.sh
+++ tests/github_test.sh
@@ -8,36 +8,54 @@ function set_up() {
 pr_number=123
 files_to_ignore=''
 ignore_line_deletions='false'
+ignore_file_deletions='false'
 
 function test_should_count_changes() {
   mock curl cat ./tests/fixtures/pull_request_api
 
-  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions")"
+  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")"
 }
 
-function test_should_count_changes_ignore_deletions() {
+function test_should_count_changes_ignore_line_deletions() {
   ignore_line_deletions='true'
 
   mock curl cat ./tests/fixtures/pull_request_api
 
-  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions")"
+  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")"
 }
 
-# NOTE: when `files_to_ignore` is set, we have to invoke the PR files API and iterate each file
+# NOTE: when `files_to_ignore` or `ignore_file_deletions` is set, we have to invoke the PR files API and iterate each file
 # one at at time. This is why the mock call is diffent in the subsequent test cases
+function test_should_count_changes_ignore_file_deletions() {
+  ignore_file_deletions='true'
+
+  mock curl cat ./tests/fixtures/pull_request_files_api
+
+  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")"
+}
+
 function test_should_ignore_files_with_glob() {
   files_to_ignore=("*.lock" ".editorconfig")
 
   mock curl cat ./tests/fixtures/pull_request_files_api
 
-  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions")"
+  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")"
 }
 
-function test_should_ignore_files_with_glob_ignore_deletions() {
+function test_should_ignore_files_with_glob_ignore_line_deletions() {
   files_to_ignore=("*.lock" ".editorconfig")
   ignore_line_deletions='true'
 
   mock curl cat ./tests/fixtures/pull_request_files_api
 
-  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions")"
+  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")"
+}
+
+function test_should_ignore_files_with_glob_ignore_file_deletions() {
+  files_to_ignore=("*.lock" ".editorconfig")
+  ignore_file_deletions='true'
+
+  mock curl cat ./tests/fixtures/pull_request_files_api
+
+  assert_match_snapshot "$(github::calculate_total_modifications "$pr_number" "${files_to_ignore[*]}" "$ignore_line_deletions" "$ignore_file_deletions")"
 }
